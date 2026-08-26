8721dc18c6fee36a908044f993c552ed58283e6c
holsoma <amosloh1911@gmail.com>
2026-08-26 14:22:34 +0800

    Add support for echo

--- src/main/java/Ted.java
+++ src/main/java/Ted.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Ted {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Ted.\n\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            System.out.println("    ____________________________________________________________");
+            if (command.equals("bye")) {
+                System.out.println("     Bye. Hope to see you again soon!");
+            } else {
+                System.out.println("     " + command);
+            }
+            System.out.println("    ____________________________________________________________");
+
+            if (command.equals("bye")) {
+                break;
+            }
+        }
     }
 }
