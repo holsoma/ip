8bf575302a0f21342636cc43baaa2fd57b0edf95
holsoma <amosloh1911@gmail.com>
2026-08-26 13:06:41 +0800

    Rename main class and Add greeting

---
+++ src/main/java/Ted.java
@@ -0,0 +1,6 @@
+public class Ted {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Ted.\n\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
